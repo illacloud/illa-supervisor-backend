01a1718fa1cf137b2f8256155044860b1a542a8f
karminski <code.karminski@outlook.com>
2023-03-07 17:15:29 +0800

    REMOVING unnecessary field.

--- src/model/get_user_by_id_response.go
+++ src/model/get_user_by_id_response.go
@@ -8,29 +8,26 @@ import (
 )
 
 type GetUserByIDResponse struct {
-	ID           string    `json:"id"`
-	UID          uuid.UUID `json:"uid"`
-	TeamMemberID string    `json:"teamMemberID"`
-	Nickname     string    `json:"nickname"`
-	Email        string    `json:"email"`
-	Avatar       string    `json:"avatar"`
-	Language     string    `json:"language"`
-	IsSubscribed bool      `json:"isSubscribed"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID        string    `json:"id"`
+	UID       uuid.UUID `json:"uid"`
+	Nickname  string    `json:"nickname"`
+	Email     string    `json:"email"`
+	Avatar    string    `json:"avatar"`
+	Language  string    `json:"language"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 func NewGetUserByIDResponse(user *User) *GetUserByIDResponse {
 	customization := user.ExportUserCustomization()
 	resp := &GetUserByIDResponse{
-		ID:           idconvertor.ConvertIntToString(user.ID),
-		UID:          user.UID,
-		Email:        user.Email,
-		Avatar:       user.Avatar,
-		Language:     customization.Language,
-		IsSubscribed: customization.IsSubscribed,
-		CreatedAt:    user.CreatedAt,
-		UpdatedAt:    user.UpdatedAt,
+		ID:        idconvertor.ConvertIntToString(user.ID),
+		UID:       user.UID,
+		Email:     user.Email,
+		Avatar:    user.Avatar,
+		Language:  customization.Language,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
 	}
 	// fill nickname
 	if user.Nickname == PENDING_USER_NICKNAME { // reset pending user name for frontend display
@@ -39,10 +36,6 @@ func NewGetUserByIDResponse(user *User) *GetUserByIDResponse {
 		resp.Nickname = user.Nickname
 	}
 	return resp
-}
-
-func (resp *GetUserByIDResponse) SetTeamMemberID(id int) {
-	resp.TeamMemberID = idconvertor.ConvertIntToString(id)
 }
 
 func (resp *GetUserByIDResponse) ExportForFeedback() interface{} {
